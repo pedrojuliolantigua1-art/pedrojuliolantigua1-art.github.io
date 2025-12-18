0505741681149928d31a78c348559662726bcc5d
pedrojuliolantigua1-art <pedrojuliolantigua1@gmail.com>
2025-12-18 11:53:31 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+const main_img = document.querySelector('.main_img')
+const thumbnails1 = document.querySelectorAll('.thumbnail1')
+const thumbnails2 = document.querySelectorAll('.thumbnail2')
+const thumbnails3 = document.querySelectorAll('.thumbnail3')
+const thumbnails4 = document.querySelectorAll('.thumbnail4')
+
+//Este javascript es el del cambio de las imagenes cada Thumbnail es lo que permite que funcionen simplemente cambia la imagen de cada uno
+//en la parte de abajo donde dice main_img.src = "Aqui pones la imagen que va al dar clic a ese boton"
+thumbnails1.forEach(thumb => {
+    thumb.addEventListener('click', function(){
+        const active = document.querySelector('.active')
+        active.classList.remove('active')
+        thumb.classList.add('active')
+        main_img.src  = "Video/Victoria’s Secret.mp4"
+    })
+})
+
+thumbnails2.forEach(thumb => {
+    thumb.addEventListener('click', function(){
+        const active = document.querySelector('.active')
+        active.classList.remove('active')
+        thumb.classList.add('active')
+        main_img.src = "Video/lorealvid.mp4"
+    })
+})
+
+thumbnails3.forEach(thumb => {
+    thumb.addEventListener('click', function(){
+        const active = document.querySelector('.active')
+        active.classList.remove('active')
+        thumb.classList.add('active')
+        main_img.src = "Video/Mac.mp4";
+    })
+})
+
+thumbnails4.forEach(thumb => {
+    thumb.addEventListener('click', function(){
+        const active = document.querySelector('.active')
+        active.classList.remove('active')
+        thumb.classList.add('active')
+        main_img.src = "Video/maybellinevid.mp4"
+    })
+})
